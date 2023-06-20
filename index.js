7a2c63b4aa2324618d2f162f259e699c14fef7d8
Jose <josehilario27@hotmail.com>
2023-06-20 16:44:06 -0400

    added the origin to cors

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'https://callertrackerclient-production.up.railway.app/'
+}));
 
 const db = new sqlite3.Database(':memory:'); // Use in-memory database
 
